4a4818f6c5f823d9db1aa307271ce7628081291e
edungan <edungan@indeed.com>
2023-12-05 13:21:36 -0800

    Initial Commit

---
+++ MenuBarTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MenuBarTest
+//
+//  Created by Ed Dungan on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenuBarTest/MenuBarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarTestApp.swift
+//  MenuBarTest
+//
+//  Created by Ed Dungan on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
